6126eadfcb53df62c537b7e0e17312d772984b4f
hishira <okami0124@gmail.com>
2025-10-12 22:26:03 +0200

    Fix

--- frontend/src/app/features/user-list/user-list.component.ts
+++ frontend/src/app/features/user-list/user-list.component.ts
@@ -1,6 +1,12 @@
-import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
+import { ChangeDetectionStrategy, Component, computed, inject, Signal, signal } from "@angular/core";
 import { usersListMocks } from "../../core/mocks/user.mocks";
 import { MatTableModule } from "@angular/material/table";
+import { toSignal } from "@angular/core/rxjs-interop";
+import { Store } from "@ngrx/store";
+import { userSelector } from "../../store/user";
+import { User } from "../../shared/models/auth.model";
+const adminColumns = ['id', 'firstName', 'lastName', 'email', 'role', 'state'];
+const otherColumns = ['firstName', 'lastName', 'email', 'role']
 @Component({
     selector: "app-user-list",
     templateUrl: './user-list.component.html',
@@ -10,5 +16,8 @@ import { MatTableModule } from "@angular/material/table";
     styleUrl: './user-list.component.scss'
 })
 export class UserListComponent{
-    users = signal(usersListMocks)
+    readonly users = signal(usersListMocks)
+     readonly user: Signal<User | any> = toSignal(inject(Store).select(userSelector));
+    readonly columns: Signal<string[]> = computed(()=>this.user()?.role?.roleType?.roleType === 'admin' ? adminColumns : otherColumns);
+
 }
