9b4a6345fa2d0dad31cdd5eb01d6a2cc807479a3
hishira <okami0124@gmail.com>
2026-01-07 22:51:56 +0100

    Fix

--- frontend/src/app/core/components/empty-state/empty-state.component.ts
+++ frontend/src/app/core/components/empty-state/empty-state.component.ts
@@ -1,11 +1,14 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
-import { MatIconModule } from "@angular/material/icon";
+import { ChangeDetectionStrategy, Component, input, InputSignal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-    selector: 'app-empty-state',
-    templateUrl: './empty-state.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: true,
-    imports:[MatIconModule],
+  selector: 'app-empty-state',
+  templateUrl: './empty-state.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [MatIconModule],
 })
-export class EmptyStateComponent {}
+export class EmptyStateComponent {
+  readonly label: InputSignal<string> = input.required<string>();
+  readonly isEmpty: InputSignal<boolean> = input.required<boolean>();
+}
