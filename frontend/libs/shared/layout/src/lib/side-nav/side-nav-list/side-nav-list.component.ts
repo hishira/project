afe239345168b8d97dc5737e35fd4d59fd5f2903
hishira <okami0124@gmail.com>
2026-06-10 22:07:22 +0200

    Fix

--- frontend/libs/shared/layout/src/lib/side-nav/side-nav-list/side-nav-list.component.ts
+++ frontend/libs/shared/layout/src/lib/side-nav/side-nav-list/side-nav-list.component.ts
@@ -1,9 +1,5 @@
 import { Component, inject, output } from "@angular/core";
 import { MatIconModule } from "@angular/material/icon";
-import { Router } from "@angular/router";
-import { firstValueFrom } from "rxjs";
-import { AuthenticationService } from "../../services/auth.service";
-import { SnackBar } from "../../services/snack-bar.service";
 import { SIDE_NAV_ELEMENTS, SideNavElementComponent } from "./side-nav-element/side-nav-element.component";
 
 @Component({
@@ -13,24 +9,10 @@ import { SIDE_NAV_ELEMENTS, SideNavElementComponent } from "./side-nav-element/s
     imports: [MatIconModule, SideNavElementComponent],
 })
 export class SideNavListComponent {
-    // private authService = inject(AuthenticationService);
-    // private snackBar = inject(SnackBar);
-    // private router = inject(Router);
     readonly sideNavElements = inject(SIDE_NAV_ELEMENTS);
     readonly closed = output<void>();
 
     close(): void {
         this.closed.emit();
-    }
-
-    async logout(): Promise<void> {
-        // try {
-        //     await firstValueFrom(this.authService.logout());
-        //     this.snackBar.openSuccess('Logged out successfully');
-        //     this.router.navigate(['/auth/login']);
-        // } catch (error) {
-        //     console.error('Logout error:', error);
-        //     this.snackBar.openError('Unable to log out. Please try again.');
-        // }
     }
 }
